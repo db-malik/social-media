52c41cc533cc62c0af4883ad341272670e04e001
Malik <dbmalek92@gmail.com>
2022-12-13 16:29:09 +0100

    style(login , register):
    update style

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import Login from './pages/login/Login'
-import './app.scss'
 import Register from './pages/register/Register'
+import Home from './pages/home/Home'
+
+import './app.scss'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/login',
+      element: <Login />,
+    },
+    {
+      path: '/register',
+      element: <Register />,
+    },
+  ])
   return (
     <div className="app">
-      {/* <Login /> */}
-      <Register />
+      <RouterProvider router={router} />
     </div>
   )
 }
