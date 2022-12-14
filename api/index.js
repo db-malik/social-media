c197a69f1c067c6cd4b4384736be0a2bfe81afaf
Malik <dbmalek92@gmail.com>
2022-12-14 11:59:17 +0100

    feat(server route): add register, login, logout features
    - user authentification model, controller
    - user registration : add new yser to database
    - user authentification : login and generate token  sent in cookie
    - user logout

--- api/index.js
+++ api/index.js
@@ -1,9 +1,27 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
 import userRoutes from './routes/users.js'
+import authRoutes from './routes/auths.js'
+import postRoutes from './routes/posts.js'
+import commentRoutes from './routes/comments.js'
+import likeRoutes from './routes/likes.js'
 
 const app = express()
+dotenv.config()
 
+//midellware
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/posts', postRoutes)
+app.use('/api/comments', commentRoutes)
+app.use('/api/likes', likeRoutes)
 
 app.listen(8080, () => {
   console.log('server running')
